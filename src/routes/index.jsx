033958368c122e6c5776e98e6128fbec30ebd398
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-15 22:17:39 +0600

    [DONE] rootLayout-routes-setup

---
+++ src/routes/index.jsx
@@ -0,0 +1,44 @@
+import RootLayout from "@/layouts/RootLayout";
+import Contact from "@/pages/Contact";
+import Home from "@/pages/Home";
+import Login from "@/pages/Login";
+import Menu from "@/pages/Menu";
+import NotFound from "@/pages/NotFound";
+import Register from "@/pages/Register";
+import Shop from "@/pages/Shop";
+import { createBrowserRouter } from "react-router-dom";
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/contact-us",
+        element: <Contact />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default routes;
